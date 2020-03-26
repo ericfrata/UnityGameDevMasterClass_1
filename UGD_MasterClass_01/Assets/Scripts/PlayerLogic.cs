db8231de9bf41ac495518f5b5e873c99309c5f41
Eric Fratantonio <er.frata@gmail.com>
2020-03-26 09:43:44 -0400

    Finish CLASS 1

---
+++ UGD_MasterClass_01/Assets/Scripts/PlayerLogic.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLogic : MonoBehaviour
+{
+    CharacterController m_characteController;
+    
+    float m_movimentSpeed = 1.0f;
+
+    float m_horizontalInput; 
+
+    float m_jumpHeight = 0.5f;
+    float m_gravity = 0.045f; 
+    bool m_jump = false; 
+
+
+    public Vector3 m_moviment; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_characteController = GetComponent<CharacterController>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       m_horizontalInput = Input.GetAxis("Horizontal");
+
+       if(!m_jump && Input.GetButtonDown("Jump"))
+       {
+           m_jump = true; 
+       }
+
+    }
+
+    void FixedUpdate() 
+    {
+        m_moviment.x = m_horizontalInput * m_movimentSpeed * Time.deltaTime;
+        
+        // Apply gravity
+        if(!m_characteController.isGrounded){
+            if(m_moviment.y > 0)
+            {
+                m_moviment.y -= m_gravity;
+            }
+            else
+            {
+                 m_moviment.y -= m_gravity * 1.5f;
+            }
+            
+        }
+        else
+        {
+            m_moviment.y = 0;
+        }
+        
+        //Setting jumpHeight to moviment y
+
+        if(m_jump)
+        {
+            m_moviment.y = m_jumpHeight;
+            m_jump = false;
+        }
+      
+
+
+        if(m_characteController)
+        {
+            m_characteController.Move(m_moviment);
+        }
+    }
+}
---
+++ UGD_MasterClass_01/Assets/Scripts/SphereLogic.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereLogic : MonoBehaviour
+{
+       Rigidbody m_Rigidbody;
+
+    void Start()
+    {
+        m_Rigidbody = GetComponent<Rigidbody>();
+
+        //This locks the RigidBody so that it does not move or rotate in the Z axis.
+        // m_Rigidbody.constraints = RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezeRotationZ;
+    }
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            m_Rigidbody.AddForce(new Vector3(0, 500, 0));
+
+        }
+    }
+}
